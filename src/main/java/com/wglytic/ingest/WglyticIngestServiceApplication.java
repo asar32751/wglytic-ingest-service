429804ddaed45d4a710ab094afc35b0c36658ded
asar32751 <arifmohammad2798@gmail.com>
2026-02-21 00:20:03 +0530

    initial project setup

---
+++ src/main/java/com/wglytic/ingest/WglyticIngestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wglytic.ingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WglyticIngestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WglyticIngestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wglytic/ingest/WglyticIngestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wglytic.ingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WglyticIngestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
